7bd3d11e7e7b7654b38294bf06087e15c6071316
steedredd2023 <sore4773@colorado.edu>
2022-05-13 18:01:40 -0600

    finish conversion of cpush, debug and compile next

--- TimeTest/TimingCode/fortranTest.f90
+++ TimeTest/TimingCode/fortranTest.f90
@@ -2,6 +2,7 @@ subroutine testcase
     use iso_c_binding
 
     implicit none
+    
 
     real, dimension(100) :: test1
     real, dimension(100,100) :: test2
@@ -31,29 +32,29 @@ subroutine testcase
     end interface
 
     integer  i,j,k
-
+    
     type(c_ptr) :: ptrtimer
     call initptr(ptrtimer)
     call starttimerf(ptrtimer)
-    do i = 1, 100
-        test1(i) = test1(i) + 1
+    do i = 1, 99
+        test1(i) = test1(i)
     end do
-    call stoptimerf(ptrtimer)
-    call savetimesf(ptrtimer)
-    
-    do i = 1, 100
-        do j = 1,100
-            test2(i,j) = test2(i,j) + 1
-        end do
-    end do
-
-    do i = 1, 100
-        do j = 1,100
-            do k = 1,100
-                test3(i,j,k) = test3(i,j,k) + 1
-            end do
-        end do
-    end do
+    !call stoptimerf(ptrtimer)
+    !call savetimesf(ptrtimer)
+    !print *,'1'
+    !do i = 1, 100
+    !    do j = 1,100
+    !        test2(i,j) = test2(i,j) + 1
+    !    end do
+    !end do
+!
+    !do i = 1, 100
+    !    do j = 1,100
+    !        do k = 1,100
+    !            test3(i,j,k) = test3(i,j,k) + 1
+    !        end do
+    !    end do
+    !end do
 
 end subroutine testcase
 
